cfb7ccb7731ff008d12a0937ca6016d4639addb4
Sean Sundberg <seansund@us.ibm.com>
2021-07-14 18:29:51 -0500

    Fixes name
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource null_resource setup_gitops {
   depends_on = [null_resource.create_yaml]
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-gitops.sh 'gitops-config' '${local.yaml_dir}' 'namespace/${var.namespace}' '${local.application_branch}' '${var.namespace}'"
+    command = "${path.module}/scripts/setup-gitops.sh 'namespace-${var.namespace}' '${local.yaml_dir}' 'namespace/${var.namespace}' '${local.application_branch}' '${var.namespace}'"
 
     environment = {
       GIT_CREDENTIALS = jsonencode(nonsensitive(var.git_credentials))
